7827bcfe353b93ef0ae4970ef265860d5def8c9c
Om Prakash <bomprakash485@gmail.com>
2026-03-06 20:14:13 +0530

    add the UI using gradio

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from google import genai
-from dotenv import load_dotenv
 from google.genai import types
+from dotenv import load_dotenv
+import gradio as gr
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
@@ -14,20 +15,29 @@ languages = {
     "French": "Translate the given sentence into French."
 }
 
-def language_translator(user_prompt, language):
+def language_translator(question, language):
     system_prompt = languages[language]
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         config=types.GenerateContentConfig(
-            system_instruction=system_prompt, 
-            temperature=0.3, 
+            system_instruction=system_prompt,
+            temperature=0.3,
             max_output_tokens=2000
         ),
-        contents=user_prompt
+        contents=question
     )
     return response.text
-sentence = "Welcome to the course Building LLM Applications"
-language = "Hindi"
 
-output = language_translator(sentence, language)
-print(output)
+demo = gr.Interface(
+    fn=language_translator, 
+    inputs=[
+        gr.Textbox(lines=4, placeholder="Enter a sentence to translate...", label="Input Sentence"), 
+        gr.Radio(choices=list(languages.keys()), value="Hindi", label="Target Language")
+    ], 
+    outputs=gr.Textbox(lines=10, label="Translated Output"),
+    title="Language Translator",
+    description="Translate sentences to Hindi, Telugu, or French using AI."
+)
+
+demo.launch()
